0743873ef80f2373975adbdc6beac1c51b34a5b7
ashishcloudinfra <ashish.cloud.infra@gmail.com>
2016-05-08 11:58:40 +0530

    test

--- cookbooks/starter/recipes/default.rb
+++ cookbooks/starter/recipes/default.rb
@@ -3,7 +3,6 @@
 
 log "Welcome to Chef, #{node["starter_name"]}!" do
   level :info
-end
-hello
+  
 
 # For more information, see the documentation: https://docs.chef.io/essentials_cookbook_recipes.html
